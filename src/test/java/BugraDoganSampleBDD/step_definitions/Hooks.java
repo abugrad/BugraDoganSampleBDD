6d8b7e5c72fc7be631f5dc45b058c97a4480dc16
Bugra Dogan <57418728+abugrad@users.noreply.github.com>
2024-01-10 09:23:06 -0600

    Add files via upload

---
+++ src/test/java/BugraDoganSampleBDD/step_definitions/Hooks.java
@@ -0,0 +1,31 @@
+package BugraDoganSampleBDD.step_definitions;
+
+import BugraDoganSampleBDD.utilities.ConfigurationReader;
+import BugraDoganSampleBDD.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        Driver.get().manage().window().maximize();
+        Driver.get().get(ConfigurationReader.get("url"));
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        //takes screenshot if the scenario is failed and adds it into report under target file
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+        Driver.closeDriver();
+    }
+
+
+}
